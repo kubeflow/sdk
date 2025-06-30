7b3bd852a44f178671d0631ca345433de29990f2
Electronic-Waste <2690692950@qq.com>
2025-06-30 12:22:47 +0000

    fix(sdk): update relative path judgement.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- python/kubeflow/trainer/utils/utils.py
+++ python/kubeflow/trainer/utils/utils.py
@@ -372,7 +372,7 @@ def get_args_using_torchtune_config(
         parts = storage_uri_parsed.path.strip("/").split("/")
         relative_path = "/".join(parts[1:]) if len(parts) > 1 else "."
 
-        if "." in relative_path:
+        if relative_path != "." and "." in relative_path:
             args.append(
                 f"dataset.data_files={os.path.join(constants.DATASET_PATH, relative_path)}"
             )
