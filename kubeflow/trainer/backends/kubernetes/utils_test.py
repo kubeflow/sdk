6d5191dcc41104242c27c8077a324c60e97c6f9d
krishdef7 <gargkrish06@gmail.com>
2026-02-26 20:16:20 +0530

    fix: pre-commit formatting
    Signed-off-by: krishdef7 <gargkrish06@gmail.com>

--- kubeflow/trainer/backends/kubernetes/utils_test.py
+++ kubeflow/trainer/backends/kubernetes/utils_test.py
@@ -741,4 +741,3 @@ def test_get_args_from_dataset_preprocess_config(test_case: TestCase):
         assert test_case.expected_status == FAILED
         assert type(e) is test_case.expected_error
     print("test execution complete")
-    
